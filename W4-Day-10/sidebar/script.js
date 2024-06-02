c4750e7a652fd5458df81fd031bac83d8ff3b187
TheKiruls <amirin856@gmail.com>
2024-06-02 17:25:12 +0800

    sidebar completed

--- W4-Day-10/sidebar/script.js
+++ W4-Day-10/sidebar/script.js
@@ -1,15 +1,28 @@
+// assign dom elements to variables
 const btnMenu = document.querySelector("#menuIcon");
-const sideBar = document.querySelector("#sidebar");
 const closeIcon = document.querySelector("#xIcon");
+const sideBar = document.querySelector("#sidebar");
+const sidebarBackground = document.querySelector("#sidebarBackground");
 
+// addEventListener(a, b)
+// a: event name
+// b: function to run when event happens
 btnMenu.addEventListener("click", function () {
     console.log("menu button clicked");
     // sideBar.style.display = "block"
-    sideBar.style.right = "0"
+    sidebarBackground.style.visibility = "visible";
+    sideBar.style.right = "0" 
 });
 
 closeIcon.addEventListener("click", function () {
     console.log("menu button clicked");
     // sideBar.style.display = "none"
+    sidebarBackground.style.visibility = "hidden";
     sideBar.style.right = "-200px"
-})
+});
+
+sidebarBackground.addEventListener ("click", function () {
+    console.log("background clicked");
+    sidebarBackground.style.visibility = "hidden";
+    sidebarBackground.style.right = "-200px";
+});
