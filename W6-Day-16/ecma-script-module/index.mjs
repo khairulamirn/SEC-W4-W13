151c4bea71d246583b4ec9053f1941efa8e00aba
TheKiruls <amirin856@gmail.com>
2024-06-25 20:22:03 +0800

    w6-day-16

---
+++ W6-Day-16/ecma-script-module/index.mjs
@@ -0,0 +1,8 @@
+// const math = require('./math.js');
+import math from './math.mjs';
+
+const addAnswer = (math.add(16, 26));
+const substractAnswer = (math.substract(16, 26));
+
+console.log(addAnswer);
+console.log(substractAnswer);
---
+++ W6-Day-16/ecma-script-module/math.mjs
@@ -0,0 +1,17 @@
+function add (a, b) {
+    return a + b;
+};
+
+function substract (a, b) {
+    return a - b;
+};
+
+const math = {
+    add,
+    substract
+};
+
+// module.exports = math;
+
+// using export default
+export default math;
