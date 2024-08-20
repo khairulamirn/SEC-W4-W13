d841ab58b1f1b6321101d2202d39419399aab760
TheKiruls <amirin856@gmail.com>
2024-08-20 21:51:21 +0800

    update

--- React-Day-2/kerja-it-clone/src/pages/Home.jsx
+++ React-Day-2/kerja-it-clone/src/pages/Home.jsx
@@ -3,15 +3,24 @@ import { useNavigate } from "react-router-dom"
 function Home() {
     const navigate = useNavigate()
     // Handler function - to navigate to view job
-    function navigateToViewJob() {
-        navigate('/job')
+    function navigateToViewJob(id) {
+        navigate(`/job/${id}`);
     }
     return (
         <div>
             <h1>Home</h1>
-            <button onClick={navigateToViewJob}>
-                View job 1
-            </button>
+            <button onClick={
+                //  this is how we call the handler function with parameter in JSX
+                function(){
+                    navigateToViewJob(1)
+                }}
+            >View job 1</button>
+            <button onClick={() => navigateToViewJob(2)}>View job 2</button>
+            <button onClick={() => navigateToViewJob(3)}>View job 3</button>
+            <button onClick={() => navigateToViewJob(4)}>View job 4</button>
+            <button onClick={() => navigateToViewJob(5)}>View job 5</button>
+            <button onClick={() => navigateToViewJob(6)}>View job 6</button>
+            <button onClick={() => navigateToViewJob(7)}>View job 7</button>
         </div>
     );
 }
--- React-Day-2/kerja-it-clone/src/pages/ViewJob.jsx
+++ React-Day-2/kerja-it-clone/src/pages/ViewJob.jsx
@@ -6,10 +6,13 @@ function ViewJob() {
         navigate('/')
     }
 
-    const useParams = useParams()
+    const params = useParams();
+    // console.log(params);
+    const jobId = params.id;
+    // console.log(jobId);
     return (
         <div>
-            <h1>View Job</h1>
+            <h1>View Job {jobId}</h1>
             <button onClick={navigateToHome}>
                 Back to Home
             </button>
