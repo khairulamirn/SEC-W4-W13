7da7bbe04545b07dc67b1d8731acd8834af2f01b
TheKiruls <amirin856@gmail.com>
2024-08-21 14:48:50 +0800

    continue with data

--- React-Day-2/kerja-it-clone/src/pages/Home.jsx
+++ React-Day-2/kerja-it-clone/src/pages/Home.jsx
@@ -1,6 +1,15 @@
 import { useNavigate } from "react-router-dom"
 
 function Home() {
+    const jobs = [
+        { id: 1, title: "Fullstack Developer" },
+        { id: 2, title: "Frontend Developer" },
+        { id: 3, title: "Backend Developer" },
+        { id: 4, title: "Data Scientist" },
+        { id: 5, title: "Data Analyst" },
+        { id: 6, title: "Data Engineer" },
+        { id: 7, title: "Data Architect" },
+    ];
     const navigate = useNavigate()
     // Handler function - to navigate to view job
     function navigateToViewJob(id) {
@@ -9,7 +18,8 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <button onClick={
+
+            {/* <button onClick={
                 //  this is how we call the handler function with parameter in JSX
                 function(){
                     navigateToViewJob(1)
@@ -20,7 +30,22 @@ function Home() {
             <button onClick={() => navigateToViewJob(4)}>View job 4</button>
             <button onClick={() => navigateToViewJob(5)}>View job 5</button>
             <button onClick={() => navigateToViewJob(6)}>View job 6</button>
-            <button onClick={() => navigateToViewJob(7)}>View job 7</button>
+            <button onClick={() => navigateToViewJob(7)}>View job 7</button> */}
+
+            {/* list all jobs dynamically from the array */}
+            {jobs.map(function (job) {
+                return (
+                    <div style={{
+                        border: "1px solid black", 
+                        marginBottom: "10px", 
+                        padding: "10px"
+                        }}
+                    >
+                        <h3>{job.title}</h3>
+                        <button onClick={() => navigateToViewJob(job.id)}>View Job</button>
+                    </div>
+                )
+            })}
         </div>
     );
 }
