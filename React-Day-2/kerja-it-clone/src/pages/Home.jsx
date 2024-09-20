05b0fc0126f607795e7c1b2cddca30776a8592b5
TheKiruls <amirin856@gmail.com>
2024-09-20 21:40:12 +0800

    20/9

--- React-Day-2/kerja-it-clone/src/pages/Home.jsx
+++ React-Day-2/kerja-it-clone/src/pages/Home.jsx
@@ -1,51 +1,36 @@
-import { useNavigate } from "react-router-dom"
 
 function Home() {
-    const jobs = [
-        { id: 1, title: "Fullstack Developer" },
-        { id: 2, title: "Frontend Developer" },
-        { id: 3, title: "Backend Developer" },
-        { id: 4, title: "Data Scientist" },
-        { id: 5, title: "Data Analyst" },
-        { id: 6, title: "Data Engineer" },
-        { id: 7, title: "Data Architect" },
-    ];
-    const navigate = useNavigate()
-    // Handler function - to navigate to view job
-    function navigateToViewJob(id) {
-        navigate(`/job/${id}`);
-    }
+
     return (
         <div>
-            <h1>Home</h1>
-
-            {/* <button onClick={
-                //  this is how we call the handler function with parameter in JSX
-                function(){
-                    navigateToViewJob(1)
-                }}
-            >View job 1</button>
-            <button onClick={() => navigateToViewJob(2)}>View job 2</button>
-            <button onClick={() => navigateToViewJob(3)}>View job 3</button>
-            <button onClick={() => navigateToViewJob(4)}>View job 4</button>
-            <button onClick={() => navigateToViewJob(5)}>View job 5</button>
-            <button onClick={() => navigateToViewJob(6)}>View job 6</button>
-            <button onClick={() => navigateToViewJob(7)}>View job 7</button> */}
-
-            {/* list all jobs dynamically from the array */}
-            {jobs.map(function (job) {
-                return (
-                    <div style={{
-                        border: "1px solid black", 
-                        marginBottom: "10px", 
-                        padding: "10px"
-                        }}
-                    >
-                        <h3>{job.title}</h3>
-                        <button onClick={() => navigateToViewJob(job.id)}>View Job</button>
-                    </div>
-                )
-            })}
+            <div id ="top">
+                <p>Find more remote jobs at Kerja-Remote.com</p>
+            </div>
+            <nav>
+                <h1>
+                    kerja-it.com
+                </h1>
+                <div>
+                    <button>
+                        📊 Stats
+                    </button>
+                    <button>
+                        🏹 Talents
+                    </button>
+                    <button>
+                        📢 Post Jobs
+                    </button>
+                    <button>
+                        💌 Get Job Alerts
+                    </button>
+                    <button>
+                        ☁️ Login
+                    </button>
+                    <button>
+                        ✨ Register
+                    </button>
+                </div>
+            </nav>
         </div>
     );
 }
